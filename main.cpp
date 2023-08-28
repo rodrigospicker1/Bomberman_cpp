0ad720952a615074c003645838413ff4b9d72f63
rodrigospicker1 <r.spicker13@gmail.com>
2023-08-28 01:39:53 -0300

    parede_bomba_cenario

--- main.cpp
+++ main.cpp
@@ -6,14 +6,44 @@
 #include<windows.h>
 #include<conio.h>
 #include <time.h>
+#include <ctime>
+#include <stdlib.h>
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
 #define GREEN   "\033[32m"
 #define YELLOW  "\033[33m"
 #define BLUE    "\033[34m"
+int linhas = 15;
+int colunas = 15;
 
 using namespace std;
+
+/*int constroiMatriz(int &matriz){
+
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            if(i == 0 || j == 0 || i == linhas-1 || j == colunas-1){ //BORDAS
+                matriz[i][j] = 1;
+            }else
+            {
+                if(i % 2 == 0 || j% 2 == 0){ //Tijolos do meio fixo
+                    matriz[i][j] = 1;
+                }else
+                {
+                    if((i == 1 && (j == 1 || j ==2)) || (j == 1 && i == 2) || (i == linhas-2 && (j == colunas-3 || j == colunas-2)) || (i == linhas-3 && j == colunas-2)){ //Parte início jogador
+                        matriz[i][j] = 0;
+                    }else{
+                        &matriz[i][j] = (rand()% 2) + 2;
+                    }
+                }
+            }
+        }
+    }
+
+    return matriz;
+
+} */
 
 
 int main()
@@ -35,51 +65,55 @@ int main()
     ///ALERTA: N�O MODIFICAR O TRECHO DE C�DIGO, ACIMA.
 
     clock_t inicio, fim;
+    srand(time(NULL));
 
-    int m[15][15]={ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                };
-
-
-    //Posi��o inicial do personagem no console
-    int x=5, y=5;
     //Vari�vel para tecla precionada
     char tecla;
     bool tempo = false;
+    int x = 1;
+    int y = 1;
+
+
+    int matriz[linhas][colunas]=
+    {
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,0,0,2,2,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,2,1,0,1,0,1,0,1,0,1,0,1,
+        1,2,2,2,2,0,0,0,0,0,0,0,0,0,1,
+        1,2,1,2,1,0,1,0,1,0,1,0,1,0,1,
+        1,2,2,2,0,0,0,0,0,0,0,0,0,0,1,
+        1,2,1,0,1,0,1,0,1,0,1,0,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    };
 
     while(true){
         ///Posiciona a escrita no início do console
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 
         ///Imprime o jogo: mapa e personagem.
-        for(int i=0;i<15;i++){
-            for(int j=0;j<15;j++){
+        for(int i=0;i<linhas;i++){
+            for(int j=0;j<colunas;j++){
                 if(i==x && j==y){
                     cout<<char(2); //personagem
                 } else {
-                    switch (m[i][j]){
+                    switch (matriz[i][j]){
                         case 0: cout<< GREEN <<char(219)<<RESET; break; //caminho
                         case 1: cout<<char(219); break; //parede
-                        case 0: cout<< BLUE <<char(219)<<RESET; break; //parede frágil
+                        case 2: cout<< BLUE <<char(219)<<RESET; break; //parede frágil
+                        case 4: cout<< RED <<char(219)<<RESET; break; //parede frágil
                         //default: cout<<"-"; //erro
                     } //fim switch
                 }
             }
             cout<<"\n";
-        } //fim for mapa
+        }
 
         ///executa os movimentos
          if ( _kbhit() ){
@@ -87,16 +121,27 @@ int main()
             switch(tecla)
             {
                 case 72: case 'w': ///cima
-                    x--;
+                    if(matriz[x-1][y] == 0){
+                        x--;
+                    }
                 break;
                 case 80: case 's': ///baixo
-                    x++;
+                    if(matriz[x+1][y] == 0){
+                        x++;
+                    }
                 break;
                 case 75:case 'a': ///esquerda
-                    y--;
+                    if(matriz[x][y-1] == 0){
+                        y--;
+                    }
                 break;
                 case 77: case 'd': ///direita
-                    y++;
+                    if(matriz[x][y+1] == 0){
+                        y++;
+                    }
+                break;
+                case 32: case 'space': ///bomba
+                    matriz[x][y] = 4;
                 break;
             }
          }
